4d6b5099708a32563e87b9ec08d8e3d660a3765b
Anakin-Skywalter <90532812+Anakin-Skywalter@users.noreply.github.com>
2022-03-23 16:41:45 +0200

    Developing the storage functionality for a to-do list application.

--- dom-methods-properties/to-do-list-function/index.js
+++ dom-methods-properties/to-do-list-function/index.js
@@ -1,10 +1,5 @@
 'use strict';
 
-const jsdom = require("jsdom");
-const dom = new JSDOM(`<!DOCTYPE html><body><p id="main">My First JSDOM!</p></body>`);
-// This prints "My First JSDOM!"
-console.log(dom.window.document.getElementById("main").textContent);
-
 // function adding elements into to-do list
 
 const tasks = [
